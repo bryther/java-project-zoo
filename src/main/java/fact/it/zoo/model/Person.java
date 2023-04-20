65c5642242972d92201c402c9cee2a013f536097
ChristiaenA <r0949046@student.thomasmore.be>
2023-04-20 17:35:43 +0200

    added variables, constructors, getters, setters, and toString function to Person

--- src/main/java/fact/it/zoo/model/Person.java
+++ src/main/java/fact/it/zoo/model/Person.java
@@ -1,4 +1,33 @@
 package fact.it.zoo.model;
 
 public class Person {
+    private String firstName;
+    private String surName;
+
+    public Person(){}
+    public Person(String fn, String sn){
+        firstName = fn;
+        surName = sn;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String name){
+        firstName = name;
+    }
+    public String getSurName(){
+        return surName;
+    }
+    public void setSurName(String name){
+        surName = name;
+    }
+    public String toString(){
+        String sn = surName.toUpperCase();
+        String fn = firstName;
+        String name = sn + " " + fn;
+        return name;
+    }
+
+
 }
+
